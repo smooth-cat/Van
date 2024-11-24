b08e447feaca63aa8a6e0bb3ca387447272895c8
milong <987444013@qq.com>
2024-11-25 02:22:44 +0800

    feat: 切换到rspack打包

---
+++ front/rspack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const rspack = require('@rspack/core');
+
+module.exports = {
+	experiments: {
+    css: true,
+  },
+	output: {
+    path: path.resolve(__dirname, '../dist'),
+		filename: 'index.js',
+		library: {
+			name: 'CodeGuide',
+      type: 'umd',
+    },
+  },
+	externals: {
+    vscode: 'vscode',
+  },
+	entry: './front/index.ts',
+  resolve: {
+    tsConfig: {
+      configFile: path.resolve(__dirname,  '../tsconfig.rollup.json'),
+    },
+    extensions: ['...', '.ts'],
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'builtin:swc-loader',
+            options: {
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                },
+              },
+            },
+          },
+        ],
+      },
+			{
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'less-loader',
+          },
+        ],
+        type: 'css',
+      },
+			// {
+      //   test: /\.css$/i,
+      //   use: [rspack.CssExtractRspackPlugin.loader, 'css-loader'],
+      //   type: 'javascript/auto',
+      // },
+    ],
+  },
+	watch: true,
+};
---
+++ src/rspack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+module.exports = {
+	target: 'node18.12',
+	output: {
+    path: path.resolve(__dirname, '../dist-ext'),
+		filename: 'extension.js',
+		library: {
+      type: 'commonjs',
+    },
+  },
+	externals: {
+    vscode: 'vscode',
+  },
+	entry: './src/extension.ts',
+  resolve: {
+    tsConfig: {
+      configFile: path.resolve(__dirname,  '../tsconfig.rollup-src.json'),
+    },
+    extensions: ['...', '.ts'],
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'builtin:swc-loader',
+            options: {
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                },
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+	watch: true,
+};
