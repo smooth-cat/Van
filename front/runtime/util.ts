a9f06e0014008952f4a6ece06af4cf75c2edbc2d
milong <987444013@qq.com>
2025-01-20 11:06:49 +0800

    feat: 新增 cName api 辅助类名书写

--- front/runtime/util.ts
+++ front/runtime/util.ts
@@ -1,2 +1,19 @@
 export const isObject = val => val !== null && typeof val === 'object';
-export const hasOwn = (target: any, key: any) => Object.prototype.hasOwnProperty.call(target, key)
+export const hasOwn = (target: any, key: any) => Object.prototype.hasOwnProperty.call(target, key);
+
+
+type NameItem = Record<string, any> | string;
+export const cNames = (...args: NameItem[]) => {
+	return args.reduce((value, item) => {
+		if(typeof item === 'string') {
+			return value + ' ' + item;
+		}
+		for (const key in item) {
+			const bool = item[key];
+			if(bool) {
+				value += ' ' + key;
+			}
+		}
+		return value;
+	}, '')
+}
