1bea5df786f5b87b6857258491c361645c58c171
milong <987444013@qq.com>
2025-02-05 22:48:09 +0800

    feat: runtime新增useComputed解决computed依赖非响应式props问题

---
+++ front/runtime/use-computed.ts
@@ -0,0 +1,20 @@
+import { computed, ref } from "@vue/reactivity"
+import { onPropsChanged } from "./life-circle";
+
+export const useComputed = <T>(getter: () => T, deps: string[]) => {
+	const updateFlag = ref(0);
+	const computeValue = computed(() => {
+		// 触发 getter
+		updateFlag.value;
+		const getRes = getter();
+		return getRes;
+	});
+	const depSet = new Set(deps)
+	onPropsChanged((changedProps) => {
+		const hasKey = Object.keys(changedProps).some((key) => depSet.has(key))
+		if(hasKey) {
+			updateFlag.value++;
+		}
+	});
+	return computeValue;
+}
